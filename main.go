4016e9d1d4a324f5fc067df9032c7a32060e8417
Yuta Matsuo <y.matsuo.dev@gmail.com>
2024-05-12 08:10:52 +0900

    feat: add functions
    - can specify any isbns
    - update template design

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"html/template"
 	"io"
 	"net/http"
 	"os"
@@ -19,6 +20,15 @@ func main() {
 	}
 
 	r := gin.Default()
+	// 関数の登録
+	r.SetFuncMap(template.FuncMap{
+		"mul": func(a, b int) int {
+			return a * b
+		},
+		"add": func(a, b int) int {
+			return a + b
+		},
+	})
 	r.LoadHTMLGlob("templates/*")
 
 	r.GET("/health", func(c *gin.Context) {
@@ -26,60 +36,66 @@ func main() {
 	})
 
 	r.GET("/", func(c *gin.Context) {
-		book, err := getBookData()
+		isbns := c.QueryArray("isbns[]")
+
+		books, err := getBookData(isbns)
 		if err != nil {
 			fmt.Printf("error fetching book data, %v", err)
 			return
 		}
 
 		c.Header("Content-Type", "image/svg+xml")
-		c.HTML(http.StatusOK, "bookshelf-2.html", gin.H{
-			"items": book.Items,
-			"base64": encode(book.Items[0].Item.MediumImageUrl),
+		c.HTML(http.StatusOK, "bookshelf.html", gin.H{
+			"books": books,
+			"bookLength": len(books),
 		})
 	})
 
-	r.Run(":8080")
+	r.Run(":8088")
 }
 
 type Book struct {
-	Items []struct {
-		Item struct {
-			Title          string `json:"title"`
-			MediumImageUrl string `json:"mediumImageUrl"`
-		}
-	} `json:"items"`
-	Count int `json:"pageCount"`
+	Title       string
+	ImageBase64 string
 }
 
 // getBookData fetches book data
-func getBookData() (Book, error) {
+func getBookData(isbns []string) ([]Book, error) {
 	key := os.Getenv("RAKUTEN_BOOKS_API_KEY")
 
 	// url := "https://api.openbd.jp/v1/get?isbn=9784798178639"
-	url := "https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?format=json&outOfStockFlag=1" + "&applicationId=" + key + "&isbn=" + "9784798178189" // + "&isbn[]=" + "9784774189673"
+	// url := "https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?format=json&outOfStockFlag=1" + "&applicationId=" + key + "&isbn=" + "9784798178189" // + "&isbn[]=" + "9784774189673"
 
-	var book Book
+	var books []Book
 
-	resp, err := http.Get(url)
-	if err != nil {
-		panic(err)
+	for _, isbn := range isbns {
+		var book Book
+
+		url := "https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?format=json&outOfStockFlag=1" + "&applicationId=" + key + "&isbn=" + isbn
+
+		resp, err := http.Get(url)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			panic(err)
+		}
+
+		err = json.Unmarshal(body, &book)
+		if err != nil {
+			panic(err)
+		}
+
+		books = append(books, book)
 	}
-	defer resp.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	err = json.Unmarshal(body, &book)
-	if err != nil {
-		panic(err)
-	}
-
-	return book, nil
+	return books, nil
 }
 
+// encode encodes image to base64
 func encode(url string) string {
 	res, err := http.Get(url)
 	if err != nil {
@@ -103,4 +119,29 @@ func encode(url string) string {
 	// file.Read(data)
 
 	return base64.StdEncoding.EncodeToString(image)
+}
+
+func (book *Book) UnmarshalJSON(byte []byte) error {
+	type RakutenBook struct {
+		Items []struct {
+			Item struct {
+				Title          string `json:"title"`
+				MediumImageUrl string `json:"mediumImageUrl"`
+			}
+		} `json:"items"`
+	}
+	var rakutenBook RakutenBook
+	err := json.Unmarshal(byte, &rakutenBook)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	if len(rakutenBook.Items) == 0 {
+		return nil
+	}
+
+	book.Title = rakutenBook.Items[0].Item.Title
+	book.ImageBase64 = encode(rakutenBook.Items[0].Item.MediumImageUrl)
+
+	return err
 }
